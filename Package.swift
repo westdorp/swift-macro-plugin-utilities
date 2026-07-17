19e3af2f5727c56dce69ce1bf84c5444c0c25c07
westdorp <34314700+westdorp@users.noreply.github.com>
2026-07-17 15:56:52 -0700

    Add Sendable extension generation

--- Package.swift
+++ Package.swift
@@ -23,6 +23,7 @@ let package = Package(
             dependencies: [
                 .product(name: "SwiftDiagnostics", package: "swift-syntax"),
                 .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
             ]
         ),
